1242b9c2d1d26f030fc7fcb7ff1c908cd47e72da
h3xar0n <aron@glambyadelina.com>
2017-04-16 23:24:39 -0400

    added several major cities to list

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var main = function() {
   var cities = [
-      
+      "New York",
+      "Atlanta",
+      "Boston",
+      "San Francisco",
+      "Los Angeles",
+      "Chicago",
+      "Miami",
+      "Seattle"
     ];
     $( "#myCity" ).autocomplete({
       source: cities
